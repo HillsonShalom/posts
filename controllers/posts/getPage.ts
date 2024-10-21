8ad2416433b4e00fb9a63a33aa2b637ac19ed94b
HillsonShalom <hillshaloz@gmail.com>
2024-10-21 11:04:22 +0300

    get page

--- controllers/posts/getPage.ts
+++ controllers/posts/getPage.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
+import { Post } from "../../types/models/postSchema";
 
 export default async (
-    req: Request,
+    req: Request<{id: number}>,
     res: Response
 ) => {
     try {
-
+        const page = req.params.id;
+        const pack = 10;
+        const data = await Post.find({}).sort({ createdAt: -1 }).skip((page -1) * pack).limit(pack).exec();
+        res.json(data);
     } catch(err) {
         const error = err as AppResError
         res.status(error.statusCode || 500).json({ name: error.name, message: error.message });
